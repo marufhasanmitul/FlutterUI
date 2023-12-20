53babeea86423cc79caab803f274521129055fcc
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-12-21 00:53:42 +0600

    FirebaseAdded Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Instagram Clone',
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: webBackgroundColor
       ),
