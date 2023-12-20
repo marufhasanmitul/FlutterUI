7526aca168b121894528fd8a733cddb331419e6d
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-12-21 01:03:56 +0600

    Firebase Added web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
@@ -7,7 +8,20 @@ import 'package:instagram_clone/utils/colors.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyCWE-5IgLEW9YN80YtL6fZDog2NwF70vYE",
+          appId: "1:957025831472:web:c69b8dfc104bf7c3457bf5",
+          messagingSenderId: "957025831472",
+          projectId: "instagram-eec68",
+          storageBucket: "instagram-eec68.appspot.com"
+      )
+    );
+  }else{
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 
 }
